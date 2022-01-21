d8f0df1568d4fe8a18d30127b9869ca776135578
MarshmallowFlump <lfhlord@gmail.com>
2022-01-21 16:07:47 +0000

    Set up Heroku prod config

--- listen.js
+++ listen.js
@@ -1,7 +1,7 @@
+const { PORT = 9090 } = process.env;
 const app = require('./app');
 
-const PORT = 9090;
-
-app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}...`)
+app.listen(PORT, (err) => {
+    if (err) throw err;
+    console.log(`Server listening on port ${PORT}...`);
 });
