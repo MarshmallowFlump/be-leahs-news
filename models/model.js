88caaaaee87527b32972e3580b97b643392fd0bb
MarshmallowFlump <lfhlord@gmail.com>
2022-03-17 01:08:49 +0000

    add model for essential endpoint DELETE /api/comments/:comment_id

--- models/model.js
+++ models/model.js
@@ -154,8 +154,17 @@ exports.addArticleComment = (newComment, article_id) => {
     });
 };
 
-exports.deletedComment = () => {
-
+exports.eraseComment = (comment_id) => {
+    return db
+    .query(
+        `DELETE FROM comments
+        WHERE comment_id = $1
+        ;`,
+        [comment_id]
+    )
+    .then(( {rows} ) => {
+        return rows;
+    });
 };
 
 exports.fetchAPI = () => {
