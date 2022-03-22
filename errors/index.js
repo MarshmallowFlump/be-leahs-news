5ad9639886aee6fe8b5787056048d2ccf96e89c5
MarshmallowFlump <lfhlord@gmail.com>
2022-03-22 01:37:12 +0000

    expansion to errors index.js file to include 404, custom errors, PSQL errors and server errors

--- errors/index.js
+++ errors/index.js
@@ -1,11 +1,31 @@
-exports.handleError404 = (err, req, res, next) => {
-    console.log(err)
-    if (err.status === 404) {
-        res.status(404).send({msg: 'Not found'});
-    } else next (err);
+exports.handle404s = (req, res) => {
+    res.status(404).send({ msg: 'Invalid URL'});
 };
 
-exports.otherErrors = (err, req, res, next) => {
+exports.handleCustomErrors = (err, req, res, next) => {
+    if (err.status && err.msg) {
+        res.status(err.status).send({msg: err.msg});
+    } else {
+        next (err);
+    };
+};
+
+exports.handlePSQLErrors = (err, req, res, next) => {
+    if (
+        err.code === '22P02' ||
+        err.code === '23502' ||
+        err.code === '42703' ||
+        err.code === '42P01' ||
+        err.code === '23503'
+        ) {
+        res.status(400).send({msg: 'Invalid input'});
+    } else {
+        next (err);
+    };
+};
+
+exports.handleServerErrors = (err, req, res, next) => {
     console.log(err, '<< Error handling required')
     res.status(500).send({msg: 'Internal Server Error'});
 }
+
