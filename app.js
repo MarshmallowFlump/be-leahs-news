9997f245826c36edf5175803797794a33df89cd9
MarshmallowFlump <lfhlord@gmail.com>
2022-03-22 01:36:00 +0000

    updates to function names and notes for general readability of file and expanded error handling within app.js

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const express = require('express');
 
 const { 
     getTopics, 
-    getArticleID,
-    patchArticleID,
+    getArticleByID,
+    patchArticleByID,
     getArticles,
     getArticleComments,
     postArticleComment,
@@ -13,26 +13,36 @@ const {
 } = require('./controllers/controller');
 
 const {
-    handleError404,
-    otherErrors
+    handleCustomErrors,
+    handlePSQLErrors,
+    handleServerErrors,
+    handle404s
 } = require('./errors/index')
 
+//Initialise the express server
 const app = express();
 
+//Initialise the cors middleware
 app.use(cors());
+
+//Utilise express.json() in order to manage JSON in endpoint requests
 app.use(express.json());
 
+//Endpoints
 app.get('/api/topics', getTopics)
-app.get('/api/articles/:article_id', getArticleID)
-app.patch('/api/articles/:article_id', patchArticleID)
+app.get('/api/articles/:article_id', getArticleByID)
+app.patch('/api/articles/:article_id', patchArticleByID)
 app.get('/api/articles', getArticles)
 app.get('/api/articles/:article_id/comments', getArticleComments)
 app.post('/api/articles/:article_id/comments', postArticleComment)
 app.delete('/api/comments/:comment_id', deleteComment)
 app.get('/api', getAPI)
 
-app.use(handleError404);
-app.use(otherErrors);
+//Error handling
+app.use(handle404s);
+app.use(handleCustomErrors);
+app.use(handlePSQLErrors)
+app.use(handleServerErrors);
 
 
 module.exports = app;
