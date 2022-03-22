372c4e7035b462c2df597bf3ea2b7c7893c945e8
MarshmallowFlump <lfhlord@gmail.com>
2022-03-22 19:39:08 +0000

    update utils file to include function to validate comments for use in essential endpoint DELETE /api/comments/:comment_id error handling

--- utils/utils.js
+++ utils/utils.js
@@ -6,7 +6,7 @@ exports.validateArticleExists = (article_id) => {
     .query(`SELECT * FROM articles WHERE article_id=$1`, [article_id])
     .then(({ rows }) => {
         if (rows.length) {
-            return true
+            return true;
         } else {
             return false;
         };
@@ -19,7 +19,20 @@ exports.validateUserExists = (username) => {
     .query(`SELECT * FROM users WHERE username=$1`, [username])
     .then(({ rows }) => {
         if (rows.length) {
-            return true
+            return true;
+        } else {
+            return false;
+        };
+    });
+};
+
+//Checks against the database to see whether a comment by a given comment_id exists
+exports.validateCommentExists = (comment_id) => {
+    return db
+    .query(`SELECT * FROM comments WHERE comment_id=$1`, [comment_id])
+    .then(({ rows }) => {
+        if (rows.length) {
+            return true;
         } else {
             return false;
         };
