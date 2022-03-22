d5d7490ab8db89f7313360ccef787b4b9d47bd0b
MarshmallowFlump <lfhlord@gmail.com>
2022-03-22 18:41:59 +0000

    minor revision to errors index file

--- errors/index.js
+++ errors/index.js
@@ -2,13 +2,6 @@ exports.handle404s = (req, res) => {
     res.status(404).send({ msg: 'Invalid URL'});
 };
 
-exports.handleCustomErrors = (err, req, res, next) => {
-    if (err.status && err.msg) {
-        res.status(err.status).send({msg: err.msg});
-    } else {
-        next (err);
-    };
-};
 
 exports.handlePSQLErrors = (err, req, res, next) => {
     if (
@@ -19,6 +12,14 @@ exports.handlePSQLErrors = (err, req, res, next) => {
         err.code === '23503'
         ) {
         res.status(400).send({msg: 'Invalid input'});
+    } else {
+        next (err);
+    };
+};
+
+exports.handleCustomErrors = (err, req, res, next) => {
+    if (err.status && err.msg) {
+        res.status(err.status).send({msg: err.msg});
     } else {
         next (err);
     };
