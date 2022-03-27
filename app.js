3dc4b83522975bee9cee94eb067556ad4b905e70
MarshmallowFlump <lfhlord@gmail.com>
2022-03-27 17:01:59 +0100

    add notes and refactor to include router

--- app.js
+++ app.js
@@ -1,48 +1,32 @@
 const cors = require('cors');
 const express = require('express');
 
-const { 
-    getTopics, 
-    getArticleByID,
-    patchArticleByID,
-    getArticles,
-    getArticleComments,
-    postArticleComment,
-    deleteComment,
-    getAPI
-} = require('./controllers/controller');
-
 const {
     handleCustomErrors,
     handlePSQLErrors,
     handleServerErrors,
     handle404s
-} = require('./errors/index')
+} = require('./errors/index');
 
 //Initialise the express server
 const app = express();
 
-//Initialise the cors middleware
-app.use(cors());
-
 //Utilise express.json() in order to manage JSON in endpoint requests
 app.use(express.json());
 
-//Endpoints
-app.get('/api/topics', getTopics)
-app.get('/api/articles/:article_id', getArticleByID)
-app.patch('/api/articles/:article_id', patchArticleByID)
-app.get('/api/articles', getArticles)
-app.get('/api/articles/:article_id/comments', getArticleComments)
-app.post('/api/articles/:article_id/comments', postArticleComment)
-app.delete('/api/comments/:comment_id', deleteComment)
-app.get('/api', getAPI)
+//Initialise the cors middleware
+app.use(cors());
+
+//Set up the router
+const apiRouter = require('./routers/api.router');
+
+//Set up the /api endpoint
+app.use('/api', apiRouter);
 
 //Error handling
 app.use(handle404s);
 app.use(handleCustomErrors);
 app.use(handlePSQLErrors)
 app.use(handleServerErrors);
-
 
 module.exports = app;
